51f27e71a38d9ae4fa8f7bf553927e64cf608341
jacquikeating <j.keating@live.ca>
2025-07-04 03:32:30 -0400

    Added Header content
    - Created basic JSX elements for entire <header> section.
    - Elements:
       - Hero image
       - <h1> with name
       - job title
       - website URL
       - buttons for email and linkedin

--- digital-business-card/src/components/Header.jsx
+++ digital-business-card/src/components/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img src="#" alt="Photo of Jacqui smiling" className="photo" />
+      <h1>Jacqui Keating</h1>
+      <p className="job-title">Full-Stack Developer</p>
+      <a href="#" target="_blank" className="website">
+        jacquidoesnothaveadomainrightnow.com
+      </a>
+      <div className="buttons-container">
+        <button>
+          <a href="mailto:j.keating@live.ca">Email</a>
+        </button>
+        <button>
+          <a href="linkedin.com" target="_blank">
+            LinkedIn
+          </a>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
