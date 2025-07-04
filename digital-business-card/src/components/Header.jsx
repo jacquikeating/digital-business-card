8f9ef00062ac49c861430fa108c26988fbedc0ea
jacquikeating <j.keating@live.ca>
2025-07-04 04:08:03 -0400

    Updated header elements
    - Updated JSX element structure within Header component.
    - Corrected typo: className="website" --> "website-link".
    - Buttons are now child elements and the links are parents.
    - Imported FontAwesome <script>.
    - Placed <i> elements from FontAwesome for the email and LinkedIn icons.

--- digital-business-card/src/components/Header.jsx
+++ digital-business-card/src/components/Header.jsx
@@ -6,18 +6,23 @@ export const Header = () => {
       <img src="#" alt="Photo of Jacqui smiling" className="photo" />
       <h1>Jacqui Keating</h1>
       <p className="job-title">Full-Stack Developer</p>
-      <a href="#" target="_blank" className="website">
+      <a href="#" target="_blank" className="website-link">
         jacquidoesnothaveadomainrightnow.com
       </a>
       <div className="buttons-container">
-        <button>
-          <a href="mailto:j.keating@live.ca">Email</a>
-        </button>
-        <button>
-          <a href="linkedin.com" target="_blank">
+        <a href="mailto:j.keating@live.ca" className="btn-link">
+          <button>
+            <i className="fa-solid fa-envelope btn-icon"></i>
+            Email
+          </button>
+          l
+        </a>
+        <a href="linkedin.com" target="_blank" className="btn-link">
+          <button className="cta-btn">
+            <i className="fa-brands fa-linkedin btn-icon"></i>
             LinkedIn
-          </a>
-        </button>
+          </button>
+        </a>
       </div>
     </header>
   );
