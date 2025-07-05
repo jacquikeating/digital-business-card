909e1e9774332345f6adc21b0853dce89b6f55af
jacquikeating <j.keating@live.ca>
2025-07-05 02:33:02 -0400

    Added Footer content
    - Added content (4 links and icons) to the Footer component.
    - Exchanged .social-icon width and height for font-size.

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="twitter.com" target="_blank">
+        <i className="fa-brands fa-square-twitter social-icon"></i>
+      </a>
+      <a href="facebook.com" target="_blank">
+        <i className="fa-brands fa-square-facebook social-icon"></i>
+      </a>
+      <a href="instagram.com" target="_blank">
+        <i className="fa-brands fa-square-instagram social-icon"></i>
+      </a>
+      <a href="github.com" target="_blank">
+        <i className="fa-brands fa-square-github social-icon"></i>
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
